c02f854a87d3d6a7c441d9e01f25b1753f130ea5
Parnian <parnian@Parnians-MacBook-Pro.local>
2017-01-26 11:33:43 +0100

    cleanup completed todos link

--- main.js
+++ main.js
@@ -5,58 +5,65 @@ function toggleDone() {
 }
 
 function updateCounters() {
-  var ntodos = $(".todo").length;
-  var ncompleted = $(".completed").length
+    var ntodos = $(".todo").length;
+    var ncompleted = $(".completed").length
 
-  $("#total-count").html(ntodos);
-  $("#completed-count").html(ncompleted);
-  $("#todo-count").html(ntodos - ncompleted);
+    $("#total-count").html(ntodos);
+    $("#completed-count").html(ncompleted);
+    $("#todo-count").html(ntodos - ncompleted);
 }
 
 function submitTodo(event) {
-  // stop the form from doing the default action, submitting...
-  event.preventDefault();
-  var title = $("#new-todo").val();
-  createTodo(title);
-  $("#new-todo").val(null);
-  updateCounters();
+    // stop the form from doing the default action, submitting...
+    event.preventDefault();
+    var title = $("#new-todo").val();
+    createTodo(title);
+    $("#new-todo").val(null);
+    updateCounters();
 }
 
 
 function createTodo(title) {
-  var checkboxId = "todo-" + nextTodoId();
+    var checkboxId = "todo-" + nextTodoId();
 
-  var listItem = $("<li></li>");
-  listItem.addClass("todo");
+    var listItem = $("<li></li>");
+    listItem.addClass("todo");
 
-  var checkbox = $('<input>');
-  checkbox.attr('type', 'checkbox');
-  checkbox.attr('id', checkboxId);
-  checkbox.val(1);
-  checkbox.bind('change', toggleDone);
+    var checkbox = $('<input>');
+    checkbox.attr('type', 'checkbox');
+    checkbox.attr('id', checkboxId);
+    checkbox.val(1);
+    checkbox.bind('change', toggleDone);
 
-  var space = document.createTextNode(" ");
+    var space = document.createTextNode(" ");
 
-  var label = $('<label></label>');
-  label.attr('for', checkboxId);
-  label.html(title);
+    var label = $('<label></label>');
+    label.attr('for', checkboxId);
+    label.html(title);
 
-  listItem.append(checkbox);
-  listItem.append(space);
-  listItem.append(label);
+    listItem.append(checkbox);
+    listItem.append(space);
+    listItem.append(label);
 
-  $("#todolist").append( listItem );
+    $("#todolist").append(listItem);
 
-  updateCounters();
+    updateCounters();
 }
 
 function nextTodoId() {
-  return $(".todo").length + 1;
+    return $(".todo").length + 1;
+}
+
+function cleanUpDoneTodos(event) {
+    event.preventDefault();
+    $.when($(".completed").remove())
+        .then(updateCounters);
 }
 
 $(document).ready(function() {
-  $("input[type=checkbox]").bind('change', toggleDone);
-  updateCounters();
-});
+    $("input[type=checkbox]").bind('change', toggleDone);
+    updateCounters();
 
-$("form").bind('submit', submitTodo);
+    $("form").bind('submit', submitTodo);
+    $("#clean-up").bind('click', cleanUpDoneTodos);
+});
